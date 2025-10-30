206e322d2ee8651dc00f2c8783e8d169e1eddf29
LucasLinux <lucasmoraesbraga@gmail.com>
2025-10-29 21:32:37 -0300

    Update de agendamentos

--- controllers/controller.py
+++ controllers/controller.py
@@ -217,27 +217,38 @@ def editar_agendamento(tree):
 
     entry_ag_nome = ctk.CTkComboBox(janela_edicao, values=list(alunos_dict.keys()), width=300)
     ctk.CTkLabel(janela_edicao, text="Nome do Aluno:", anchor="w", font=ctk.CTkFont(size=size, weight="bold")).pack(fill="x", pady=(10,5), padx=20)
+    entry_ag_nome.set(item[1])
     entry_ag_nome.pack(pady=(0,5))
     entry_ag_inst = ctk.CTkComboBox(janela_edicao, values=list(inst_dict.keys()), width=200)
     ctk.CTkLabel(janela_edicao, text="Nome do Instrutor:", anchor="w", font=ctk.CTkFont(size=size, weight="bold")).pack(fill="x", pady=(10,5), padx=20)
+    entry_ag_inst.set(item[2])
     entry_ag_inst.pack(pady=(0,5))
     entry_ag_veic = ctk.CTkComboBox(janela_edicao, values=list(veic_dict.keys()), width=200)
     ctk.CTkLabel(janela_edicao, text="Veículo:", anchor="w", font=ctk.CTkFont(size=size, weight="bold")).pack(
         fill="x", pady=(10, 5), padx=20)
+    entry_ag_veic.set(item[3])
     entry_ag_veic.pack(pady=(0,5))
 
     ctk.CTkLabel(janela_edicao, text="Data (DD/MM/AAAA):", anchor="w", font=ctk.CTkFont(size=size, weight="bold")).pack(
         fill="x",pady=5, padx=20)
     entry_data = ctk.CTkEntry(janela_edicao)
+    dataF = item[4].split(" ")[0].split("-")
+    dataF.reverse()
+    dataF = "/".join(dataF)
+    entry_data.insert(0, dataF)
     entry_data.pack(anchor="w", pady=5, padx=100)
+
     ctk.CTkLabel(janela_edicao, text="Hora (HH:MM):", anchor="w", font=ctk.CTkFont(size=size, weight="bold")).pack(
         fill="x",pady=5, padx=20)
     entry_hora = ctk.CTkEntry(janela_edicao)
     entry_hora.pack(anchor="w",pady=5, padx=100)
+    entry_hora.insert(0, item[4].split(" ")[1][:5])
+
     categorias = ["A", "B", "AB"]
     entry_ag_cat = ctk.CTkComboBox(janela_edicao, values=categorias, width=150)
     ctk.CTkLabel(janela_edicao, text="Categoria:", anchor="w", font=ctk.CTkFont(size=size, weight="bold")).pack(
         fill="x", pady=(10, 5), padx=20)
+    entry_ag_cat.set(item[5])
     entry_ag_cat.pack(pady=(0, 5))
 
     def update_aula():
@@ -256,20 +267,21 @@ def editar_agendamento(tree):
             return
         try:
             newDatetime = datetime_converter(data, hora)
-            conn = ctl.conectar_banco()
+            conn = conectar_banco()
             cursor = conn.cursor()
-            sql = "INSERT INTO agendamento (id_instrutor, id_aluno, id_veiculo, datetime, cat) VALUES (%s, %s, %s, %s, %s)"
-            cursor.execute(sql, (idInst, idAluno, idVeic, newDatetime, cat))
+            sql = """UPDATE agendamento
+                     SET id_aluno = %s, id_instrutor = %s, id_veiculo = %s, datetime = %s, cat = %s
+                     WHERE id = %s"""
+            cursor.execute(sql, (idAluno, idInst, idVeic, newDatetime, cat, item[0]))
             conn.commit()
             cursor.close()
             conn.close()
-            messagebox.showinfo("Agendamento", f"Aula agendada para {nome} em {data} às {hora}")
+            messagebox.showinfo("Aula alterada", f"Aula Prática de {nome} alterada com sucesso")
         except mysql.connector.Error as erro:
             messagebox.showerror("Erro", f"Erro ao salvar no banco: {erro}")
-        entry_ag_nome.set("")
-        entry_ag_data.delete(0, ctk.END)
-        entry_ag_hora.delete(0, ctk.END)
-        atualizar_lista_agendadas()
+        janela_edicao.destroy()
+        buscar_aulas(tree)
+
 
 
     btn_alterar = ctk.CTkButton(janela_edicao, text="Fazer alterações", width=200,
